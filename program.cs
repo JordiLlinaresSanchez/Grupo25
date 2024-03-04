66d05586ca818dbe624baa1702c9c5db6d90ae6a
jordi <jorllina@ucm.es>
2024-03-04 19:47:33 +0100

    cambios

--- program.cs
+++ program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("EL VIERNES SANGRIADAAAAAAAAAAA!");// al final no hubo
+            A();
+        }
+
+        static void A() 
+        {
+            while (true)
+            {
+                if (!false) { return; }
+            }
         }
     }
 }
